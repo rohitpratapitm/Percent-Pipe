42d3ada8f0a9a4690b14dd552addb708ee9808c4
rohitpratapitm <rohitpratapitm@gmail.com>
2021-10-29 11:53:53 -0400

    Added input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,9 @@ import { Component, VERSION } from '@angular/core';
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
 
-  value: number = 5000;
-  a: number = 0.259;
-  b: number = 1.3495;
+  value: number;
   locale: string = 'en';
+
   constructor() {
     this.locale= navigator.language;
   }
