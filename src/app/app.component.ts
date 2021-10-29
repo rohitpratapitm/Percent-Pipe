6fa900e402e895ea17c886e208e9ac1695169354
rohitpratapitm <rohitpratapitm@gmail.com>
2021-10-29 12:01:00 -0400

    commented code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,13 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
-
-  value: number;
   locale: string = 'en';
 
   constructor() {
-    this.locale= navigator.language;
+    this.locale = navigator.language;
   }
 }
