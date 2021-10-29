8e5875b5e34dd399f64e4295ac6aeecd12044c30
rohitpratapitm <rohitpratapitm@gmail.com>
2021-10-29 11:41:02 -0400

    Added Italian locale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,9 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { registerLocaleData } from '@angular/common';
 import localeFr from '@angular/common/locales/fr';
+import localeIt from '@angular/common/locales/it';
 registerLocaleData(localeFr, 'fr');
+registerLocaleData(localeIt, 'it');
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, HelloComponent ],
